6c1c051e1704c3e5e50f560ffa3a3cfbbd37fdfb
ALMLAMBO <aleksm007148@gmail.com>
2023-12-30 21:52:54 +0200

    Configure connection to database and register servlet for db connection test

--- back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/BackendApplication.java
+++ back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/BackendApplication.java
@@ -1,17 +1,16 @@
 package com.rateuni.backend;
 
+import com.rateuni.backend.servlets.TestDbConnection;
 import com.rateuni.backend.servlets.HelloWorldServlet;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.List;
 
 @SpringBootApplication
-@Configuration
 public class BackendApplication {
 
     public static void main(String[] args) {
@@ -23,6 +22,14 @@ public class BackendApplication {
         ServletRegistrationBean<HelloWorldServlet> srb = new ServletRegistrationBean<>();
         srb.setServlet(new HelloWorldServlet());
         srb.setUrlMappings(List.of("/hello-world-servlet"));
+        return srb;
+    }
+
+    @Bean
+    ServletRegistrationBean<TestDbConnection> asyncHelloWorldServlet() {
+        ServletRegistrationBean<TestDbConnection> srb = new ServletRegistrationBean<>();
+        srb.setServlet(new TestDbConnection());
+        srb.setUrlMappings(List.of("/test-db-connection"));
         return srb;
     }
 }
