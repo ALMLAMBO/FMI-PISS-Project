41c1e5128d2210fe501e793f13f6166ccf7220b0
ALMLAMBO <aleksm007148@gmail.com>
2023-12-31 14:32:50 +0200

    Add data models for degree and course

---
+++ back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/models/Course.java
@@ -0,0 +1,122 @@
+package com.rateuni.backend.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "courseName")
+    private String courseName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "credits")
+    private double credits;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "lecturer")
+    private String lecturer;
+
+    @Column(name = "assistants")
+    private String assistants;
+
+    public Course() {
+    }
+
+    public Course(int id, String courseName, String description,
+                  double credits, String type, String lecturer, String assistants) {
+
+        this.id = id;
+        this.courseName = courseName;
+        this.description = description;
+        this.credits = credits;
+        this.type = type;
+        this.lecturer = lecturer;
+        this.assistants = assistants;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        this.credits = credits;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public String getAssistants() {
+        return assistants;
+    }
+
+    public void setAssistants(String assistants) {
+        this.assistants = assistants;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id
+                && Double.compare(credits, course.credits) == 0
+                && Objects.equals(courseName, course.courseName)
+                && Objects.equals(description, course.description)
+                && Objects.equals(type, course.type)
+                && Objects.equals(lecturer, course.lecturer)
+                && Objects.equals(assistants, course.assistants);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, courseName, description, credits, type, lecturer, assistants);
+    }
+}
---
+++ back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/models/Degree.java
@@ -0,0 +1,68 @@
+package com.rateuni.backend.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "degrees")
+public class Degree {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    public Degree() {
+    }
+
+    public Degree(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Degree degree = (Degree) o;
+        return id == degree.id
+                && Objects.equals(title, degree.title)
+                && Objects.equals(description, degree.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description);
+    }
+}
