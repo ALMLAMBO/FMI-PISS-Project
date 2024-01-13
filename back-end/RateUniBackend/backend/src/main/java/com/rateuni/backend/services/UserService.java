8ef8c97696159885efb41b913fffd38c8e6e4fad
ALMLAMBO <aleksm007148@gmail.com>
2024-01-13 23:51:10 +0200

    Rewrite method for creating review

--- back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/services/UserService.java
+++ back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/services/UserService.java
@@ -1,21 +1,13 @@
 package com.rateuni.backend.services;
 
-import com.google.cloud.firestore.DocumentReference;
 import com.rateuni.backend.models.base_models.*;
 import com.rateuni.backend.models.link_models.*;
-import com.rateuni.backend.repositories.base_repos.*;
-import com.rateuni.backend.repositories.link_repos.*;
-import jakarta.transaction.Transactional;
-import org.hibernate.boot.model.naming.IllegalIdentifierException;
-import org.javatuples.Quintet;
-import org.javatuples.Triplet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
 @Service
 public class UserService extends BaseService {
@@ -98,6 +90,16 @@ public class UserService extends BaseService {
         return disciplines;
     }
 
+    public UniUser getUser(int userId) throws ExecutionException, InterruptedException {
+        return firestore
+                .collection(CollectionsNames.USERS_COLLECTION_NAME)
+                .whereEqualTo("id", userId)
+                .get()
+                .get()
+                .toObjects(UniUser.class)
+                .get(0);
+    }
+
     public void createUser(int universityId, int facultyId, int degreeId, UniUser user) {
         University university;
         Faculty faculty;
@@ -149,135 +151,55 @@ public class UserService extends BaseService {
         });
     }
 
-//    @Transactional
-//    public void registerUser(int universityId, int facultyId, int degreeId, UniUser user) {
-//        try {
-//            Triplet<University, Faculty, Degree> uniData = checkUniData(universityId, facultyId, degreeId);
-//            Role role = new Role();
-//            role.setRole("student");
-//
-//            uniUserRepository.save(user);
-//            universityUserRepository.save(new UniversityUser(uniData.getValue0(), user));
-//            facultyUserRepository.save(new FacultyUser(uniData.getValue1(), user));
-//            userDegreeRepository.save(new UserDegree(user, uniData.getValue2()));
-//            userRoleRepository.save(new UserRole(user, role));
-//
-//            List<Discipline> requiredDisciplines = degreeDisciplineRepository
-//                    .findAllById(List.of(degreeId))
-//                    .stream()
-//                    .map(DegreeDiscipline::getDiscipline)
-//                    .toList();
-//
-//            for (Discipline requiredDiscipline : requiredDisciplines) {
-//                userDisciplineRepository.save(new UserDiscipline(user, requiredDiscipline));
-//            }
-//        }
-//        catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
-//
-//    @Transactional
-//    public void createReview(int universityId, int facultyId, int degreeId, int disciplineId, int userId, Review review) {
-//        try {
-//            UniUser user = checkUserData(universityId, facultyId, degreeId, disciplineId, userId);
-//            reviewService.createReview(disciplineId, review);
-//        }
-//        catch (IllegalArgumentException e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
-//
-//    private Triplet<University, Faculty, Degree> checkUniData(int universityId, int facultyId, int degreeId) {
-//        University university = universityRepository
-//                .findById(universityId)
-//                .get();
-//
-//        if(university == null) {
-//            throw new IllegalArgumentException("User Service register user: Invalid university id");
-//        }
-//
-//        Faculty faculty = facultyRepository
-//                .findById(facultyId)
-//                .get();
-//
-//        if(faculty == null) {
-//            throw new IllegalArgumentException("User Service register user: Invalid university id");
-//        }
-//
-//        Degree degree = degreeRepository
-//                .findById(degreeId)
-//                .get();
-//
-//        if(degree == null) {
-//            throw new IllegalArgumentException("User Service register user: Invalid degree id");
-//        }
-//
-//        return new Triplet<>(university, faculty, degree);
-//    }
-//
-//    private UniUser checkUserData(
-//            int universityId, int facultyId, int degreeId, int disciplineId, int userId) {
-//
-//        Triplet<University, Faculty, Degree> uniData = checkUniData(universityId, facultyId, degreeId);
-//        Discipline discipline = disciplineRepository
-//                .findById(disciplineId)
-//                .get();
-//
-//        if(discipline == null) {
-//            throw new IllegalArgumentException("User Service create review: Invalid discipline id");
-//        }
-//
-//        UniUser user = uniUserRepository
-//                .findById(userId)
-//                .get();
-//
-//        if(user == null) {
-//            throw new IllegalArgumentException("User Service create review: Invalid user id");
-//        }
-//
-//        int expectedUniId = universityUserRepository
-//                .findById(userId)
-//                .get()
-//                .getUniversity()
-//                .getId();
-//
-//        if(expectedUniId != universityId) {
-//            throw new IllegalArgumentException("User Service create review: User is not from this university");
-//        }
-//
-//        int expectedFacultyId = facultyUserRepository
-//                .findById(userId)
-//                .get()
-//                .getFaculty()
-//                .getId();
-//
-//        if(expectedFacultyId != facultyId) {
-//            throw new IllegalArgumentException("User Service create review: User is not from this faculty");
-//        }
-//
-//        int expectedDegreeId = userDegreeRepository
-//                .findById(userId)
-//                .get()
-//                .getDegree()
-//                .getId();
-//
-//        if(expectedDegreeId != degreeId) {
-//            throw new IllegalArgumentException("User Service create review: User is not from this degree");
-//        }
-//
-//        int expectedDisciplineId = userDisciplineRepository
-//                .findAllById(List.of(userId))
-//                .stream()
-//                .filter(x -> x.getDiscipline().getId() == disciplineId)
-//                .map(x -> x.getDiscipline().getId())
-//                .toList()
-//                .get(0);
-//
-//        if(expectedDisciplineId != disciplineId) {
-//            throw new IllegalArgumentException("User Service create review: User is not from this discipline");
-//        }
-//
-//        return user;
-//    }
+    public void createReview(int universityId, int facultyId, int degreeId, int disciplineId, int userId, Review review) {
+        University university;
+        Faculty faculty;
+        Degree degree;
+        Discipline discipline;
+        UniUser user;
+
+        try {
+            university = universityService.getUniversity(universityId);
+            faculty = facultyService.getFaculty(facultyId);
+            degree = degreeService.getDegree(degreeId);
+            discipline = disciplineService.getDiscipline(disciplineId);
+            user = getUser(userId);
+        }
+        catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        firestore.runAsyncTransaction(x -> {
+            ReviewRequest reviewRequest = new ReviewRequest();
+            try {
+                int prevReviewId = getId(CollectionsNames.REVIEWS_COLLECTION_NAME);
+                review.setId(prevReviewId + 1);
+                updateId(CollectionsNames.REVIEWS_COLLECTION_NAME);
+                review.setVisible(false);
+                review.setStatus("pending");
+
+                int prevRequestId = getId(CollectionsNames.REVIEWS_REQUESTS_COLLECTION_NAME);
+                reviewRequest.setRequestId(prevRequestId + 1);
+                updateId(CollectionsNames.REVIEWS_REQUESTS_COLLECTION_NAME);
+
+                reviewRequest.setReviewId(review.getId());
+                reviewRequest.setUsername(user.getUsername());
+                reviewRequest.setDegree(degree.getTitle());
+                reviewRequest.setDiscipline(discipline.getCourseName());
+                reviewRequest.setStatus("pending");
+                reviewRequest.setApproved(false);
+            }
+            catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            firestore
+                    .collection(CollectionsNames.REVIEWS_COLLECTION_NAME)
+                    .add(review);
+
+            return firestore
+                    .collection(CollectionsNames.REVIEWS_REQUESTS_COLLECTION_NAME)
+                    .add(reviewRequest);
+        });
+    }
 }
