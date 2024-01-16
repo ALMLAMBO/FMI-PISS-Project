fb1928f365a6b634a922adab7d2398336252ec3d
ALMLAMBO <aleksm007148@gmail.com>
2024-01-16 19:17:57 +0200

    Add test controllers to test role-base authorization

---
+++ back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/servlets/test/AuthController.java
@@ -0,0 +1,38 @@
+package com.rateuni.backend.servlets.test;
+
+import com.rateuni.backend.models.base_models.UniUser;
+import com.rateuni.backend.models.request_response.request.LoginRequest;
+import com.rateuni.backend.models.request_response.request.RefreshTokenRequest;
+import com.rateuni.backend.models.request_response.request.RegisterRequest;
+import com.rateuni.backend.models.request_response.response.JwtAuthenticationResponse;
+import com.rateuni.backend.models.request_response.response.JwtTokenResponse;
+import com.rateuni.backend.services.authentication.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UniUser> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authenticationService.register(registerRequest));
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<JwtTokenResponse> register(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authenticationService.login(loginRequest));
+    }
+
+    @GetMapping("/refresh")
+    public ResponseEntity<JwtAuthenticationResponse> register(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return ResponseEntity.ok(authenticationService.refreshToken(refreshTokenRequest));
+    }
+}
