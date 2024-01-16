b293e5670aaca9b3c9f1c02f28359f57da587811
ALMLAMBO <aleksm007148@gmail.com>
2024-01-16 21:00:28 +0200

    Remove test servlet

--- back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/BackendApplication.java
+++ back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/BackendApplication.java
@@ -1,25 +1,12 @@
 package com.rateuni.backend;
 
-import com.rateuni.backend.servlets.test.HelloWorldServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-    }
-
-    @Bean
-    ServletRegistrationBean<HelloWorldServlet> helloWorldServlet() {
-        ServletRegistrationBean<HelloWorldServlet> srb = new ServletRegistrationBean<>();
-        srb.setServlet(new HelloWorldServlet());
-        srb.setUrlMappings(List.of("/hello-world-servlet"));
-        return srb;
     }
 }
