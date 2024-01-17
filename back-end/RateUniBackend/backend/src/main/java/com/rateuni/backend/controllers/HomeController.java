daba8f4706db87cc9db7be71820acc4179d7e01f
ALMLAMBO <aleksm007148@gmail.com>
2024-01-17 10:55:43 +0200

    Add urls for getting data for registration

--- back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/controllers/HomeController.java
+++ back-end/RateUniBackend/backend/src/main/java/com/rateuni/backend/controllers/HomeController.java
@@ -1,10 +1,18 @@
 package com.rateuni.backend.controllers;
 
+import com.rateuni.backend.models.base_models.Degree;
+import com.rateuni.backend.models.base_models.Faculty;
 import com.rateuni.backend.models.base_models.Review;
+import com.rateuni.backend.models.base_models.University;
+import com.rateuni.backend.models.request_response.request.UniversityIdRequest;
+import com.rateuni.backend.services.business_logic.DegreeService;
+import com.rateuni.backend.services.business_logic.FacultyService;
 import com.rateuni.backend.services.business_logic.ReviewService;
+import com.rateuni.backend.services.business_logic.UniversityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +24,27 @@ import java.util.concurrent.ExecutionException;
 @RequiredArgsConstructor
 public class HomeController {
     private final ReviewService reviewService;
+    private final UniversityService universityService;
+    private final FacultyService facultyService;
+    private final DegreeService degreeService;
 
     @GetMapping("/get-reviews")
     public ResponseEntity<List<Review>> getReviews() throws ExecutionException, InterruptedException {
         return ResponseEntity.ok(reviewService.getAllReviews());
+    }
+
+    @GetMapping("/get-universities")
+    public ResponseEntity<List<University>> getAllUniversities() throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(universityService.getAllUniversities());
+    }
+
+    @GetMapping("/get-faculties")
+    public ResponseEntity<List<Faculty>> getFacultiesForUniversity(@RequestBody UniversityIdRequest request) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(facultyService.getAllFacultiesForUniversity(request.getId()));
+    }
+
+    @GetMapping("/get-degrees")
+    public ResponseEntity<List<Degree>> getDegreesForFaculty(@RequestBody UniversityIdRequest request) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(degreeService.getAllDegreesForFaculty(request.getId()));
     }
 }
